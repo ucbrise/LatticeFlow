8448116aa764e8a13b5cc2cbe2b48e2f9a0f00cd
Michael Whittaker <mjw297@cornell.edu>
2016-10-01 06:46:39 +0000

    Proof of concept: coroutine to implement iterator.
    Implementing iterators is neither fun [1] nor easy. Even simple
    operations, like the cross product, are much more difficult to implement
    as an iterator than you would as a normal function. More complex
    iterators like the merge part of a sort merge join are way, way more
    complicated.
    
    In my undergraduate compilers course, our professor mentioned that
    coroutines are a good way to implement iterators. They not only make the
    programs easier to write and read, but they are also typically very
    fast. In fact, he argued that using them can often lead to code that's
    faster than the hand-written code. I believe it; the hand written way is
    kind of crazy.
    
    In this commit, I use (one of) Boost's coroutine libraries [2] to
    for a proof of concept to see if we can use coroutines to implement
    iterators. I reimplemented the cross product using coroutines, and
    things seem to work well! I think if I implemented a more complicated
    iterator, the effects would be even more pronounced. Though things work,
    I'm still not convinced coroutines are the way to go. The boost library
    is a bit undocumented, and there are some performance penalties
    associated with them [3]. So, we should think about it more.
    
    [1]: Okay, it's pretty fun :P
    [2]: http://www.boost.org/doc/libs/1_61_0/libs/coroutine2/doc/html/index.html
    [3]: http://www.boost.org/doc/libs/1_61_0/libs/coroutine2/doc/html/coroutine2/performance.html

--- src/flow/cross.h
+++ src/flow/cross.h
@@ -1,7 +1,10 @@
 #ifndef FLOW_CROSS_H_
 #define FLOW_CROSS_H_
 
+#include <memory>
 #include <utility>
+
+#include <boost/coroutine2/all.hpp>
 
 #include "flow/operator.h"
 
@@ -11,52 +14,53 @@ template <typename A, typename B>
 class Cross : public Operator<std::pair<A, B>> {
  public:
   Cross(Operator<A>* const left, Operator<B>* const right)
-      : left_(left), right_(right) {}
+      : left_(left),
+        right_(right),
+        coroutine_([this](push_type& yield) { NextCoroutine(yield); }),
+        coroutine_iterator_(begin(coroutine_)) {}
   Cross(const Cross<A, B>&) = delete;
   Cross& operator=(const Cross<A, B>&) = delete;
 
   boost::optional<std::pair<A, B>> next() override {
-    // Initially, `left_->next()` has not been advanced, so `left_val_` will be
-    // empty when this method is called for the very first time.
-    if (!left_val_) {
-      left_val_ = left_->next();
-
-      // If `left_` is empty, `left_->next()` will never return anything.
-      if (!left_val_) {
-        return {};
-      }
+    if (coroutine_iterator_ != end(coroutine_)) {
+      std::pair<A, B> p = *coroutine_iterator_;
+      ++coroutine_iterator_;
+      return p;
+    } else {
+      return {};
     }
-
-    boost::optional<B> right_val = right_->next();
-    if (!right_val) {
-      // If we've exhausted `right_`, then we need to advance `left_` and reset
-      // `right_`. If `left_` is also exhausted, then we're done.
-      left_val_ = left_->next();
-      if (!left_val_) {
-        return {};
-      }
-
-      right_->reset();
-      right_val = right_->next();
-      // If `right_` is empty, calling `next` after `reset` won't return
-      // anything.
-      if (!right_val) {
-        return {};
-      }
-    }
-
-    return std::make_pair(*left_val_, *right_val);
   }
 
   void reset() override {
     left_->reset();
     right_->reset();
+    coroutine_ = pull_type([this](push_type& yield) { NextCoroutine(yield); });
+    coroutine_iterator_ = begin(coroutine_);
   }
 
  private:
+  using coroutine_type = boost::coroutines2::coroutine<std::pair<A, B>>;
+  using push_type = typename coroutine_type::push_type;
+  using pull_type = typename coroutine_type::pull_type;
+  using iterator = typename pull_type::iterator;
+
+  void NextCoroutine(typename coroutine_type::push_type& yield) {
+    boost::optional<A> left_val = left_->next();
+    while (left_val) {
+      right_->reset();
+      boost::optional<B> right_val = right_->next();
+      while (right_val) {
+        yield(std::make_pair(*left_val, *right_val));
+        right_val = right_->next();
+      }
+      left_val = left_->next();
+    }
+  }
+
   Operator<A>* const left_;
   Operator<B>* const right_;
-  boost::optional<A> left_val_;
+  pull_type coroutine_;
+  iterator coroutine_iterator_;
 };
 
 }  // namespace latticeflow
