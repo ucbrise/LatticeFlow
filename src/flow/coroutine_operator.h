aff984f28c8330bcf1db2808f0c06ff2a23b8788
Michael Whittaker <mjw297@cornell.edu>
2016-10-05 22:02:34 +0000

    Added base CoroutineOperator base class.

---
+++ src/flow/coroutine_operator.h
@@ -0,0 +1,54 @@
+#ifndef FCOROUTINE_LOW_OPERATOR_H_
+#define FCOROUTINE_LOW_OPERATOR_H_
+
+#include <memory>
+#include <tuple>
+
+#include "boost/coroutine2/all.hpp"
+#include "boost/optional.hpp"
+
+#include "flow/operator.h"
+
+namespace latticeflow {
+
+// TODO(mwhittaker): Document.
+template <typename... Ts>
+class CoroutineOperator : public Operator<Ts...> {
+ public:
+  // TODO(mwhittaker): Document why we can't construct the coroutine_ with the
+  // correct lambda yet.
+  CoroutineOperator()
+      : coroutine_([this](push_type& yield) {}),
+        coroutine_iterator_(begin(coroutine_)) {}
+
+  boost::optional<std::tuple<Ts...>> next() override {
+    if (coroutine_iterator_ != end(coroutine_)) {
+      std::tuple<Ts...> p = *coroutine_iterator_;
+      ++coroutine_iterator_;
+      return p;
+    } else {
+      return {};
+    }
+  }
+
+  virtual void reset() override {
+    coroutine_ = pull_type([this](push_type& yield) { YieldNext(yield); });
+    coroutine_iterator_ = begin(coroutine_);
+  }
+
+ protected:
+  using coroutine_type = boost::coroutines2::coroutine<std::tuple<Ts...>>;
+  using push_type = typename coroutine_type::push_type;
+  using pull_type = typename coroutine_type::pull_type;
+  using iterator = typename pull_type::iterator;
+
+ private:
+  virtual void YieldNext(push_type& yield) = 0;
+
+  pull_type coroutine_;
+  iterator coroutine_iterator_;
+};
+
+};  // namespace latticeflow
+
+#endif  // FLOW_COROUTINE_OPERATOR_H_
--- src/flow/cross.h
+++ src/flow/cross.h
@@ -6,6 +6,7 @@
 
 #include <boost/coroutine2/all.hpp>
 
+#include "flow/coroutine_operator.h"
 #include "flow/operator.h"
 #include "flow/template_helpers.h"
 
@@ -14,45 +15,29 @@ namespace latticeflow {
 // http://stackoverflow.com/a/21023429/3187068
 // TODO(mwhittaker): Understand this, then document it.
 template <typename Left, typename Right>
-class Cross {};
+class Cross;
 
 template <typename... Lefts, typename... Rights>
 class Cross<left<Lefts...>, right<Rights...>>
-    : public Operator<Lefts..., Rights...> {
+    : public CoroutineOperator<Lefts..., Rights...> {
  public:
   Cross(Operator<Lefts...>* const left, Operator<Rights...>* const right)
-      : left_(left),
-        right_(right),
-        coroutine_([this](push_type& yield) { NextCoroutine(yield); }),
-        coroutine_iterator_(begin(coroutine_)) {}
+      : left_(left), right_(right) {
+    reset();
+  }
   Cross(const Cross<left<Lefts...>, right<Rights...>>&) = delete;
   Cross& operator=(const Cross<left<Lefts...>, right<Rights...>>&) = delete;
-
-  boost::optional<std::tuple<Lefts..., Rights...>> next() override {
-    if (coroutine_iterator_ != end(coroutine_)) {
-      std::tuple<Lefts..., Rights...> p = *coroutine_iterator_;
-      ++coroutine_iterator_;
-      return p;
-    } else {
-      return {};
-    }
-  }
 
   void reset() override {
     left_->reset();
     right_->reset();
-    coroutine_ = pull_type([this](push_type& yield) { NextCoroutine(yield); });
-    coroutine_iterator_ = begin(coroutine_);
+    CoroutineOperator<Lefts..., Rights...>::reset();
   }
 
  private:
-  using coroutine_type =
-      boost::coroutines2::coroutine<std::tuple<Lefts..., Rights...>>;
-  using push_type = typename coroutine_type::push_type;
-  using pull_type = typename coroutine_type::pull_type;
-  using iterator = typename pull_type::iterator;
+  using push_type = typename CoroutineOperator<Lefts..., Rights...>::push_type;
 
-  void NextCoroutine(typename coroutine_type::push_type& yield) {
+  void YieldNext(push_type& yield) override {
     boost::optional<std::tuple<Lefts...>> left_val = left_->next();
     while (left_val) {
       right_->reset();
@@ -67,8 +52,6 @@ class Cross<left<Lefts...>, right<Rights...>>
 
   Operator<Lefts...>* const left_;
   Operator<Rights...>* const right_;
-  pull_type coroutine_;
-  iterator coroutine_iterator_;
 };
 
 }  // namespace latticeflow
