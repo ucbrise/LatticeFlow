34dd05ea49dadc7d3cc904a965af90c62a4a9eb6
Michael Whittaker <mjw297@cornell.edu>
2016-11-14 06:02:33 +0000

    Done with everything except dedup by group.

--- src/click/example_server.cc
+++ src/click/example_server.cc
@@ -2,6 +2,7 @@
 #include <chrono>
 #include <ios>
 #include <iostream>
+#include <numeric>
 #include <random>
 #include <string>
 #include <thread>
@@ -36,7 +37,8 @@ constexpr int kNumWorkers = 4;
 constexpr std::chrono::nanoseconds kDelay = std::chrono::seconds(5);
 
 // Returns a random integer drawn uniformally in the range [0, high]. `high`
-// must be non-negative.
+// must be non-negative. Stolen from [1].
+// [1]: http://www.cppsamples.com/common-tasks/roll-a-die.html
 int roll_die(const int high) {
   assert(high >= 0);
   std::random_device random_device;
@@ -51,26 +53,28 @@ EnvMsg&& print_envmsg(EnvMsg&& e) {
   return std::move(e);
 };
 
+// Pretty prints a `std::pair<Cid, int>`.
 std::pair<CId, int>&& print_envmsg_group(std::pair<CId, int>&& p) {
   std::cout << p.second << std::endl;
   return std::move(p);
 }
 
+// `make_group_evnmsg({cid; msg})` returns `(cid, msg.size() + 1)`.
 std::pair<CId, int> make_group_evnmsg(EnvMsg env) {
   int i = std::stoi(lf::message_to_string(std::move(env.msg)));
   return {std::move(env.cid), i};
 }
 
+// Project the `cid` field of an `EnvelopedMessage`.
 CId extract_envmsg_cid(EnvMsg env) { return std::move(env.cid); }
 
-#if 0
-void print_int(int x) { std::cout << x << std::endl; };
-
-int get_num(EnvMsg&&) {
-  static int i = 0;
-  return i++;
+EnvMsg reduce_group(std::pair<CId, std::vector<int>> group) {
+  const std::vector<int>& xs = group.second;
+  int sum = std::accumulate(std::begin(xs), std::end(xs), 0);
+  EnvMsg envmsg{.cid = std::move(group.first),
+                .msg = lf::string_to_message(std::to_string(sum))};
+  return envmsg;
 }
-#endif
 
 // TODO(mwhittaker): Document.
 void length_plus_i(const int i, zmq::context_t* context) {
@@ -96,53 +100,6 @@ void length_plus_i(const int i, zmq::context_t* context) {
 }
 
 int main() {
-#if 0
-  zmq::socket_t source(context, ZMQ_PUSH);
-  const std::string source_adress = "tcp://*:5556";
-  source.bind(source_adress);
-  std::cout << "Source listening on '" << source_adress << "'." << std::endl;
-
-  zmq::socket_t sink(context, ZMQ_PULL);
-  const std::string sink_adress = "tcp://*:5557";
-  sink.bind(sink_adress);
-  std::cout << "Sink listening on '" << sink_adress << "'." << std::endl;
-
-  lf::SocketSend out(&source);
-  lf::Drop<CId&&> drop;
-  auto copy_env_f = [](EnvMsg&& env) -> EnvMsg { return lf::Clone(env); };
-  auto copy_env = lf::make_map<EnvMsg&&, EnvMsg&&>(copy_env_f, &out);
-
-  auto dup = lf::make_dup(&copy_env, NUM_THREADS);
-  auto copy_cid_f = [](EnvMsg&& e) -> CId { return e.cid.Clone(); };
-  auto copy_cid = lf::make_map<EnvMsg&&, CId&&>(std::move(copy_cid_f), &drop);
-  auto tee = lf::make_tee<EnvMsg&&>({&copy_cid, &dup});
-  auto val_to_val = lf::make_map<EnvMsg&&, EnvMsg&&>(id_val_to_val, &tee);
-  auto refref_to_val =
-      lf::make_map<EnvMsg&&, EnvMsg&&>(id_refref_to_val, &val_to_val);
-  auto refref_to_refref =
-      lf::make_map<EnvMsg&&, EnvMsg&&>(id_refref_to_refref, &refref_to_val);
-
-  lf::SocketRecv in(&router, &refref_to_refref);
-  lf::Drop<EnvMsg&&> drop_drop;
-  auto refref_to_refref2 =
-      lf::make_map<EnvMsg&&, EnvMsg&&>(id_refref_to_refref, &drop_drop);
-  lf::SocketRecv from_workers(&sink, &refref_to_refref2);
-
-  std::vector<std::thread> threads;
-  for (int i = 0; i < NUM_THREADS; ++i) {
-    threads.push_back(std::thread(double_plus_i, i, &context));
-  }
-
-  lf::Driver driver;
-  driver.RegisterEventHandler(&in);
-  driver.RegisterEventHandler(&from_workers);
-  driver.Run();
-
-  for (std::thread& thread : threads) {
-    thread.join();
-  }
-#endif
-
   // Set up sockets.
   zmq::context_t context(1);
 
@@ -162,29 +119,39 @@ int main() {
   std::cout << "Sink listening on '" << sink_address << "'." << std::endl;
 
   // Create the element graph.
+  //
   //             out
-  //          to_single message or something
-  //        every other by group
-  //         group
-  //      end    push              end
-  //                               dedup
-  //             form group        get cid
-  //             copy              copy
-  //             tee
-  //   timer     from workers
+  //              |
+  //           reduce
+  //              |
+  //     every other by group
+  //              |
+  //     ______group by________
+  //    /         |            \
+  //   end       push         end
+  //    |         |            |
+  //    |         |          dedup
+  //    |         |            |
+  //    |     form group    get cid
+  //    |         |            |
+  //    |        copy        copy
+  //    |         | ________/
+  //    |        tee
+  //    |         |
+  //  timer  from workers
   //    |
-  //   copy      to workers
-  //               dup
-  //
-  //      tee
-  //
-  //       in
-
+  //   copy   to workers
+  //     \       |
+  //      \     dup
+  //       \   /
+  //        tee
+  //         |
+  //        in
   lf::Driver driver;
 
-  lf::Drop<EnvMsg&&> drop_envmsg;
-  lf::Drop<const Group&> drop_group;
-  lf::GroupBy<CId, int, const Group&> group_by(&drop_group);
+  lf::SocketSend out(&router);
+  lf::Map<Group&&, EnvMsg, decltype(&reduce_group)> reduce(reduce_group, &out);
+  lf::GroupBy<CId, int, Group&&> group_by(&reduce);
 
   lf::Timer<CId, CId&&> timer(&driver, kDelay, &group_by.end());
   lf::Map<EnvMsg&&, CId, decltype(&extract_envmsg_cid)> extract_cid1(
@@ -226,72 +193,4 @@ int main() {
   driver.RegisterEventHandler(&from_workers);
   driver.RegisterEventHandler(&in);
   driver.Run();
-
-#if 0
-  // Create the element graph.
-  //
-  //                                 +----------------+
-  //                                 | print_int_call |
-  //                                 +--------^-------+
-  //                                          |
-  //                                      +---+---+            +-----+
-  //                                      | timer |            | out |
-  //                                      +---^---+            +--^--+
-  //                                          |                   |
-  //   +------+ +-------------------+ +-------+-------+ +------------------+
-  //   | drop | | print_envmsg_call | | envmsg_to_int | | print_envmsg_map |
-  //   +--^---+ +-------------------+ +--------^------+ +------------------+
-  //      |               |                    |                  |
-  //      +---------------+----------+---------+------------------+
-  //                                 |
-  //                              +--+--+
-  //                              | tee |
-  //                              +--^--+
-  //                                 |
-  //                              +--+--+
-  //                              | in  |
-  //                              +-----+
-  lf::Drop<EnvMsg&&> drop;
-
-  lf::Call<EnvMsg&&, decltype(&print_envmsg)> print_envmsg_call(print_envmsg);
-
-  lf::Call<const int&, decltype(&print_int)> print_int_call(print_int);
-  lf::Timer<int, const int&> timer(
-      &driver, std::chrono::nanoseconds(5000000000), &print_int_call);
-  lf::Map<EnvMsg&&, int, decltype(&get_num)> envmsg_to_int(get_num, &timer);
-
-  lf::SocketSend out(&router);
-  lf::Map<EnvMsg&&, EnvMsg, decltype(&print_envmsg)> print_envmsg_map(
-      print_envmsg, &out);
-
-  lf::Tee<EnvMsg&&, EnvMsg&&> tee(
-      {&drop, &print_envmsg_call, &envmsg_to_int, &print_envmsg_map});
-  lf::SocketRecv<EnvMsg&&> in(&router, &tee);
-
-  auto print_group = [](const std::pair<int, std::vector<int>>& p) {
-    std::cout << p.first << ": "
-              << "[";
-    for (std::size_t i = 0; i < p.second.size(); ++i) {
-      if (i != 0) {
-        std::cout << ", ";
-      }
-      std::cout << p.second[i];
-    }
-    std::cout << "]" << std::endl;
-  };
-  lf::Call<const std::pair<int, std::vector<int>>&, decltype(print_group)&>
-      print_groups(print_group);
-  lf::GroupBy<int, int, const std::pair<int, std::vector<int>>&> group_by(
-      &print_groups);
-  group_by.push().push(std::make_pair(0, 1));
-  group_by.push().push(std::make_pair(0, 2));
-  group_by.push().push(std::make_pair(0, 3));
-  group_by.push().push(std::make_pair(0, 4));
-  group_by.end().push(1);
-  group_by.end().push(0);
-
-  // Start the driver.
-  driver.RegisterEventHandler(&in);
-  driver.Run();
-#endif
 }
